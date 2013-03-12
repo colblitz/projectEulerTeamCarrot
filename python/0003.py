7fcbc0796a8ad20b07045d199af23ff01180eb96
Benjamin Lee <bleeben@gmail.com>
2013-03-12 02:45:23 -0400

    what is going on!
    i messed something up

--- python/0003.py
+++ python/0003.py
@@ -8,20 +8,21 @@ import util
 #######################
 
 # Version 01
-@profile
+#@profile
 def main01():
   number = 600851475143
-  pfactors = range(2, int(math.sqrt(number)))
+  pfactors = xrange(2, int(math.sqrt(number)))
+  pfactors = (x for x in pfactors)
   factors = []
   while pfactors:
-    f = pfactors[0]
+    f = next(pfactors)
     if number % f == 0:
       number = number/f
       factors.append(f)
-      pfactors = pfactors[1:]
-      pfactors = [x for x in pfactors if x < int(math.sqrt(number))+1]
+      #pfactors = pfactors[1:]
+      pfactors = (x for x in pfactors if x < int(math.sqrt(number))+1)
     else:
-      pfactors = [x for x in pfactors if x % f != 0]
+      pfactors = (x for x in pfactors if x % f != 0)
   factors.append(number)
   util.printAnswer(factors[-1])
 
