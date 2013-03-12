6459de6509a445a3d866113a28ca64001d8bddc2
Benjamin Lee <bleeben@gmail.com>
2013-03-12 02:47:24 -0400

    reverting 0003.py back to what it was
    that should fix stuff

--- python/0001.py
+++ python/0001.py
@@ -14,7 +14,7 @@ def main01():
   for i in xrange(1000):
     if i % 3 == 0 or i % 5 == 0:
       sum += i
-  printAnswer(sum)
+  util.printAnswer(sum)
 
 main01()
 
--- python/0003.py
+++ python/0003.py
@@ -8,21 +8,20 @@ import util
 #######################
 
 # Version 01
-#@profile
+@profile
 def main01():
   number = 600851475143
   pfactors = xrange(2, int(math.sqrt(number)))
-  pfactors = (x for x in pfactors)
   factors = []
   while pfactors:
-    f = next(pfactors)
+    f = pfactors[0]
     if number % f == 0:
       number = number/f
       factors.append(f)
-      #pfactors = pfactors[1:]
-      pfactors = (x for x in pfactors if x < int(math.sqrt(number))+1)
+      pfactors = pfactors[1:]
+      pfactors = [x for x in pfactors if x < int(math.sqrt(number))+1]
     else:
-      pfactors = (x for x in pfactors if x % f != 0)
+      pfactors = [x for x in pfactors if x % f != 0]
   factors.append(number)
   util.printAnswer(factors[-1])
 
