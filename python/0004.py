9a1b486b5494d6353eb9a5495715a7724303fcc0
Benjamin Lee <bleeben@gmail.com>
2013-03-13 00:53:28 -0400

    Addded version 2 to 0004.py
    Dunno if its faster though.

--- python/0004.py
+++ python/0004.py
@@ -10,7 +10,7 @@ import util
 #######################
 
 # Version 01
-@profile
+#@profile
 def main01():
   def isPalindrome(n):
     s = str(n)
@@ -29,4 +29,23 @@ def main01():
   # Code here
   util.printAnswer(largest)
 
+# Version 02
+#@profile
+def main02():
+  number = 600851475143
+  palindromes = xrange(997,99,-1) # possible palindromes leftmost digits
+  candidates = (n*1000+int(str(n)[::-1]) for n in palindromes)
+  while candidates:
+    #print candidates
+    f = next(candidates)
+    for i in xrange(999,99,-1):
+      for j in xrange(999,99,-1):
+        if i*j<=f:
+          break
+      if i*j==f:
+        candidates=False
+        break
+  util.printAnswer(f)
+
 main01()
+main02()
